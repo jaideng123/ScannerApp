6979c5314e50f0ce63936fbacef729f3b70aa243
Jaiden Gerig <jaideng123@yahoo.com>
2017-04-20 15:19:18 -0500

    We can now get pixel values out of the camera preview

--- app/src/main/java/com/led_on_off/led/CameraPreview.java
+++ app/src/main/java/com/led_on_off/led/CameraPreview.java
@@ -11,11 +11,23 @@ import java.io.IOException;
 import java.util.List;
 
 import static android.content.ContentValues.TAG;
+import static com.led_on_off.led.ledControl.decodeYUV420SP;
 
 public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
     private SurfaceHolder mHolder;
     private Camera mCamera;
     float mDist = 0;
+    private int[] pixels;
+    Camera.PreviewCallback cb = new Camera.PreviewCallback() {
+        @Override
+        public void onPreviewFrame(byte[] data, Camera camera) {
+            //transforms NV21 pixel data into RGB pixels
+            decodeYUV420SP(pixels, data, camera.getParameters().getPreviewSize().width,  camera.getParameters().getPreviewSize().height);
+            //Outuput the value of the top left pixel in the preview to LogCat
+            Log.i("Pixels", "The top right pixel has the following RGB (hexadecimal) values:"
+                    +Integer.toHexString(pixels[0]));
+        }
+    };
 
     public CameraPreview(Context context, Camera camera) {
         super(context);
@@ -27,12 +39,15 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
         mHolder.addCallback(this);
         // deprecated setting, but required on Android versions prior to 3.0
         mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        mCamera.setPreviewCallback(cb);
     }
 
     public void surfaceCreated(SurfaceHolder holder) {
         // The Surface has been created, now tell the camera where to draw the preview.
+        pixels = new int[mCamera.getParameters().getPreviewSize().width * mCamera.getParameters().getPreviewSize().height];
         try {
             mCamera.setPreviewDisplay(holder);
+            mCamera.setPreviewCallback(cb);
             mCamera.startPreview();
         } catch (IOException e) {
             Log.d(TAG, "Error setting camera preview: " + e.getMessage());
@@ -46,7 +61,7 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
     public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
         // If your preview can change or rotate, take care of those events here.
         // Make sure to stop the preview before resizing or reformatting it.
-
+        pixels = new int[mCamera.getParameters().getPreviewSize().width * mCamera.getParameters().getPreviewSize().height];
         if (mHolder.getSurface() == null){
             // preview surface does not exist
             return;
@@ -65,6 +80,7 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
         // start preview with new settings
         try {
             mCamera.setPreviewDisplay(mHolder);
+            mCamera.setPreviewCallback(cb);
             mCamera.startPreview();
 
         } catch (Exception e){
@@ -94,6 +110,38 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
             }
         }
         return true;
+    }
+
+    //Method from Ketai project! Not mine! See below...
+    void decodeYUV420SP(int[] rgb, byte[] yuv420sp, int width, int height) {
+
+        final int frameSize = width * height;
+
+        for (int j = 0, yp = 0; j < height; j++) {       int uvp = frameSize + (j >> 1) * width, u = 0, v = 0;
+            for (int i = 0; i < width; i++, yp++) {
+                int y = (0xff & ((int) yuv420sp[yp])) - 16;
+                if (y < 0)
+                    y = 0;
+                if ((i & 1) == 0) {
+                    v = (0xff & yuv420sp[uvp++]) - 128;
+                    u = (0xff & yuv420sp[uvp++]) - 128;
+                }
+
+                int y1192 = 1192 * y;
+                int r = (y1192 + 1634 * v);
+                int g = (y1192 - 833 * v - 400 * u);
+                int b = (y1192 + 2066 * u);
+
+                if (r < 0)                  r = 0;               else if (r > 262143)
+                    r = 262143;
+                if (g < 0)                  g = 0;               else if (g > 262143)
+                    g = 262143;
+                if (b < 0)                  b = 0;               else if (b > 262143)
+                    b = 262143;
+
+                rgb[yp] = 0xff000000 | ((r << 6) & 0xff0000) | ((g >> 2) & 0xff00) | ((b >> 10) & 0xff);
+            }
+        }
     }
 
     private void handleZoom(MotionEvent event, Camera.Parameters params) {
--- app/src/main/java/com/led_on_off/led/ledControl.java
+++ app/src/main/java/com/led_on_off/led/ledControl.java
@@ -27,7 +27,7 @@ import android.os.AsyncTask;
 
 import java.io.IOException;
 import java.util.UUID;
-
+import static android.content.ContentValues.TAG;
 
 public class ledControl extends ActionBarActivity {
 
@@ -48,6 +48,32 @@ public class ledControl extends ActionBarActivity {
     //SPP UUID. Look for it
     static final UUID myUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
 
+    static public void decodeYUV420SP(int[] rgb, byte[] yuv420sp, int width, int height) {
+        final int frameSize = width * height;
+
+        for (int j = 0, yp = 0; j < height; j++) {
+            int uvp = frameSize + (j >> 1) * width, u = 0, v = 0;
+            for (int i = 0; i < width; i++, yp++) {
+                int y = (0xff & ((int) yuv420sp[yp])) - 16;
+                if (y < 0) y = 0;
+                if ((i & 1) == 0) {
+                    v = (0xff & yuv420sp[uvp++]) - 128;
+                    u = (0xff & yuv420sp[uvp++]) - 128;
+                }
+
+                int y1192 = 1192 * y;
+                int r = (y1192 + 1634 * v);
+                int g = (y1192 - 833 * v - 400 * u);
+                int b = (y1192 + 2066 * u);
+
+                if (r < 0) r = 0; else if (r > 262143) r = 262143;
+                if (g < 0) g = 0; else if (g > 262143) g = 262143;
+                if (b < 0) b = 0; else if (b > 262143) b = 262143;
+
+                rgb[yp] = 0xff000000 | ((r << 6) & 0xff0000) | ((g >> 2) & 0xff00) | ((b >> 10) & 0xff);
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -105,7 +131,6 @@ public class ledControl extends ActionBarActivity {
         catch(Exception e){
 
         }
-
         mPreview = new CameraPreview(this, c);
         FrameLayout preview = (FrameLayout)findViewById(R.id.camera_preview);
         preview.addView(mPreview);
@@ -237,7 +262,7 @@ public class ledControl extends ActionBarActivity {
             catch (IOException e)
             {
 
-                ConnectSuccess = false;//if the try failed, you can check the exception here
+                //ConnectSuccess = false;//if the try failed, you can check the exception here
             }
             return null;
         }
